31524fc90e1f09c640c3c77f8bbdc9bce6477595
Anna Nienaber <anna.nienaber@uni-oldenburg.de>
2024-12-05 14:02:32 +0100

    day3 - fix

--- day3.py
+++ day3.py
@@ -4,21 +4,20 @@ if __name__ == '__main__':
     file = open("day3.txt", "r")
 
     # #part1
-    # lines = [line.strip() for line in file if line.strip()]
-    # nums = []
-    # mul_sum = 0
-    # for line in lines:
-    #     ops = re.findall("mul\\([0-9]+,[0-9]+\\)", line)
-    #     print(ops)
-    #     for op in ops:
-    #         nums.append(re.findall("[0-9]+", op))
-    # print(nums)
-    # for i in range(0, len(nums)):
-    #     mul_sum += int(nums[i][0]) * int(nums[i][1])
-    # print(mul_sum)
+    lines = [line.strip() for line in file if line.strip()]
+    nums = []
+    mul_sum = 0
+    for line in lines:
+        ops = re.findall("mul\\([0-9]+,[0-9]+\\)", line)
+        print(ops)
+        for op in ops:
+            nums.append(re.findall("[0-9]+", op))
+    print(nums)
+    for i in range(0, len(nums)):
+        mul_sum += int(nums[i][0]) * int(nums[i][1])
+    print(mul_sum)
 
     # part2
-    lines = [line.strip() for line in file if line.strip()]
     nums = []
     mul_sum = 0
     all_lines = ""
